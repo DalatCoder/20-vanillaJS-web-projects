4d728e182d8ab55bf468d89905411383b2299043
DalatCoder <hieuntctk42@gmail.com>
2020-07-17 10:27:16 +0700

    Add function normalizeVietnameseString

--- 10.Music-Player/utils.js
+++ 10.Music-Player/utils.js
@@ -56,3 +56,11 @@ function formatDuration(time) {
 
   return formatted;
 }
+
+function normalizeVietnameseString(string) {
+  return string
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D');
+}
