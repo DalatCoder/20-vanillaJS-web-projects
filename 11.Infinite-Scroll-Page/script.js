4ffe10bcfd39a480f2027099380d83c5e6b78fbe
DalatCoder <hieuntctk42@gmail.com>
2020-07-17 09:30:01 +0700

    11.Complete

--- 11.Infinite-Scroll-Page/script.js
+++ 11.Infinite-Scroll-Page/script.js
@@ -2,7 +2,7 @@ const postContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Fetch posts from API
@@ -36,11 +36,52 @@ async function showPost() {
   }
 }
 
+// Show loader and fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPost();
+    }, 300);
+  }, 1000);
+}
+
+// Filter posts by input
+function filterPosts(event) {
+  const term = event.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  for (const post of posts) {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.includes(term) || body.includes(term)) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  }
+}
+
 // Show initial posts
-catchException(showPost());
+catchAsyncException(showPost());
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
 
 // UTILS FUNCTION
-function catchException(func) {
+function catchAsyncException(func) {
   return (...args) => {
     func.apply(null, args).catch((err) => {
       alert(err.message);
